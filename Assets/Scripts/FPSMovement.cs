435efec5b5da9b359ec1bb50c4d52ef997da295d
code4all2 <code4allaulaonline@gmail.com>
2024-08-08 12:23:34 -0300

    movimento do player estilo fps

---
+++ Assets/Scripts/FPSMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    private float speed = 4f;
+    private Vector2 input;
+    private Vector3 direction;
+    private Transform cam;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam.eulerAngles.y, transform.eulerAngles.z);
+
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        direction = input.x * Vector3.right + input.y * Vector3.forward;
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+    }
+}
