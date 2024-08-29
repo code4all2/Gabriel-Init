733fcd50ffa6bd491c0fc5e52a7955707ec3c58c
code4all2 <code4allaulaonline@gmail.com>
2024-08-29 12:22:49 -0300

    input arma

--- Assets/Scripts/Arma.cs
+++ Assets/Scripts/Arma.cs
@@ -4,16 +4,21 @@ public class Arma : MonoBehaviour
 {
     [SerializeField] private GameObject bala;
     [SerializeField] private Transform bocaDaArma;
-
+    Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(bala, bocaDaArma.position, bocaDaArma.rotation);
+        transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bala, bocaDaArma.position, bocaDaArma.rotation);
+        }
     }
 }
--- Assets/Scripts/FPSMovement.cs
+++ Assets/Scripts/FPSMovement.cs
@@ -21,6 +21,7 @@ public class FPSMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //
         rb = GetComponent<Rigidbody>();
         cam = Camera.main.transform;
     }
