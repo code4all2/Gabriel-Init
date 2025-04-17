28f16766c0999951762edc1896e16fd86f055373
code4all2 <code4allaulaonline@gmail.com>
2025-04-17 12:04:32 -0300

    arma melhorada

--- Assets/Scripts/Arma.cs
+++ Assets/Scripts/Arma.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class Arma : MonoBehaviour
-{
-    [SerializeField] private GameObject bala;
-    [SerializeField] private Transform bocaDaArma;
+{    
     Camera cam;
-    // Start is called before the first frame update
+    [SerializeField] int damage = 10;
+    [SerializeField] int maxBullet = 10;
+    [SerializeField] float range = 100f;
+    [SerializeField] float fireRate = 15f;
+    [SerializeField] float reloadTime = 1f;
+
+    float nextTimeToFire = 0f;
+    int currentBullet;
+    bool isReloading;
+
+   
     void Start()
     {
         cam = Camera.main;
@@ -14,11 +23,34 @@ public class Arma : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(bala, bocaDaArma.position, bocaDaArma.rotation);
+            Shoot();
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Está reloading...");
+        yield return new WaitForSeconds(reloadTime);
+        currentBullet = maxBullet;
+        isReloading=false;
+    }
+
+    void Shoot()
+    {
+        currentBullet--;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            // aplicar dano
+            // efeito visuais
+            // efeitos fisicos
         }
     }
 }
